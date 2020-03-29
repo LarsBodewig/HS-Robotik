6188493643b90eab4d96fe7a2530905efb4c116b
Fan2404 <Fan2404@aol.com>
2020-03-29 17:45:38 +0200

    Added infrared sensor module

--- aufg2.py
+++ aufg2.py
@@ -1,9 +1,26 @@
-from commons import AlphaBot, Sensor
+from commons import AlphaBot
+import random
 
-
-def main(self):
+def main():
     bot = AlphaBot()
-    
+    while True:
+        moveRandom()
+        line = bot.isOnLine()
+        if line:
+            turn()
+
+def moveRandom():
+    turn = random()
+    time = random() * 10
+    if turn < 0.5:
+        bot.leftFor(time)
+    else:
+        bot.rightFor(time)
+    bot.forwardFor(0.1)
+
+def turn():
+    bot.rightFor(6)
+    bot.forwardFor(2)
 
 if __name__ == '__main__':
     main()
--- commons/alphabot.py
+++ commons/alphabot.py
@@ -1,7 +1,38 @@
 import Servo
-
+import Sensor
+import time
 
 class AlphaBot(object):
 	
 	def __init__(self):
 		self.SERVO = Servo()
+        self.SENSOR = Sensor()
+        self.calibrateSensor()
+
+    def forwardFor(self, time):
+        self.SERVO.forward()
+        time.sleep(time)
+        self.SERVO.stop()
+
+    def leftFor(self, time):
+        self.SERVO.left()
+        time.sleep(time)
+        self.SERVO.stop()
+    
+    def rightFor(self, time):
+        self.SERVO.right()
+        time.sleep(time)
+        self.SERVO.stop()
+
+    def calibrateSensor(self):
+        for i in range(0, 15):
+            self.SENSOR.calibrate()
+            self.forwardFor(0.1)
+
+    def isOnLine(self):
+        last_value, sensor_values = self.SENSOR.readLine()
+        onLine = False
+        for i in range(0, self.SENSOR.numSensors):
+            if sensor_values[i] > 200:
+                onLine = True
+        return onLine
