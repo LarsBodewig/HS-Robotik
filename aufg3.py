5e5e56a9c772944b15380c09a6ed176ee0e90f7d
Lasse Clausen <lasseclause@hotmail.de>
2020-04-02 20:04:28 +0200

    dreh dich im kreis, robot

--- aufg3.py
+++ aufg3.py
@@ -2,7 +2,10 @@ from commons import AlphaBot
 
 def main():
     bot = AlphaBot()
-    bot.CAMERA.searchCorners()
+    for i in range(0, 10):
+        result = bot.CAMERA.searchCorners()
+        print(result)
+        bot.leftFor(0.5)
 
 
 
