3494166a39726ce03b26326cb4741303f602414d
Lasse Clausen <lasseclause@hotmail.de>
2020-04-02 12:56:34 +0200

    fixing result issues, comments, debug print lines added

--- cameraImpl/test1.py
+++ cameraImpl/test1.py
@@ -5,7 +5,7 @@ from searchColorStripes import searchColorStripes
 #img = Image.open('C:\\Users\\lasse\\github\\SS_20_Robotik_T01\\cameraImpl\\trust_issues.jpg', 'r')
 f = open('C:\\Users\\lasse\\github\\SS_20_Robotik_T01\\cameraImpl\\trust_issues.jpg', "rb")
 
-scs = searchColorStripes(newMaxPixelBetweenHits=2)
+scs = searchColorStripes(newDebug=False)
 
-range1 = scs.distanceFromImgCenter(f)
+range1 = scs.drawBlackLinesOnImg(f)
 print (range1)
