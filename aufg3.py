befe7e1c37e5db6f8b2bee494f2a7216ebd57247
unknown <fan2404@aol.com>
2020-04-11 12:48:33 +0200

    Aufg3

--- aufg3.py
+++ aufg3.py
@@ -4,13 +4,17 @@ import time
 def main():
     bot = AlphaBot()
     bot.debug = True
-    while True:
-        print("new Read --> ")
-        time.sleep(2)
-        print(bot.lineAngle())
-        print("<-- end Read \n ")
-
-
+    pillars = bot.CAMERA.searchCorners()
+    smallest = 1
+    for i in range(0, len(pillars)):
+        if abs(pillars[i][3]) < smallest:
+            smallest = pillars[i][3]
+    bot.forwardUntilOnLine(15)
+    if smallest > 0:
+        bot.rightFor(2)
+    else:
+        bot.leftFor(2)
 
 if __name__ == '__main__':
+    time.sleep(4)
     main()
